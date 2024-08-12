a071cdef7429aca2cc5f47f6812b1a4c7ce4d9e0
dmtr0n <143527536+dementr0n@users.noreply.github.com>
2024-08-12 23:19:56 +0500

    Update hikka_autoinst_root.sh

--- hikka_autoinst_root.sh
+++ hikka_autoinst_root.sh
@@ -9,15 +9,15 @@ fi
 
 clear
 
-export HOME="/root"
+cd /root
 
 apt update && apt upgrade -y && apt install python3 python3-pip git libcairo2 sudo -y
 
-cd $HOME && git clone https://github.com/hikariatama/Hikka && cd $HOME/Hikka
+git clone https://github.com/hikariatama/Hikka && cd /root/Hikka
 
 mv /usr/lib/python3.*/EXTERNALLY-MANAGED /usr/lib/python3.*/EXTERNALLY-MANAGED.old
 
-cd $HOME/Hikka && pip3 install -r requirements.txt 
+cd /root/Hikka && pip3 install -r requirements.txt 
 
 cd /etc/systemd/system && echo "[Unit]
 Description = start hikka ub
@@ -33,13 +33,13 @@ WantedBy = multi-user.target" > hikka.service
 
 cd /root && mkdir scripts
 cd /root/scripts && echo "#!/bin/bash
-cd $HOME/Hikka
+cd /root/Hikka
 python3 -m hikka --root --no-web" > hikka.sh
 
 cd /root/scripts && chmod +x hikka.sh
 cd /etc/systemd/system && systemctl enable hikka.service
 
 clear
-cd $HOME/Hikka && python3 -m hikka --root --no-web 
+cd /root/Hikka && python3 -m hikka --root --no-web 
 
 
